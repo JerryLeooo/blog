ee084b5aaf20f8f72f46ae7e77605a4329642b8c
whilgeek <whilgeek@gmail.com>
2015-09-02 01:10:11 +0800

    FUCKING JEN

---
+++ deploy.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+git add .
+git commit -m "`curl -s http://whatthecommit.com | perl -p0e '($_)=m{<p>(.+?)</p>}s'`"
+git push origin master
+
+gitbook build --output=~/pages
+cd ~/pages
+git init
+git add .
+git commit -m "curl -s http://whatthecommit.com | perl -p0e '($_)=m{<p>(.+?)</p>}s'"
+git push https://jerryleooo:$GITHUB_PASS@github.com/JerryLeooo/jerryleooo.github.io.git master -f
